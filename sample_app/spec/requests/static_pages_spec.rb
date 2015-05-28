fd2e32d33bc87f50a0ea8a64ffc8c108aba343f9
Masahiko.Negishi <masahiko.negishi@litalico.co.jp>
2015-05-28 17:53:36 +0900

    finish section 3

--- sample_app/spec/requests/static_pages_spec.rb
+++ sample_app/spec/requests/static_pages_spec.rb
@@ -9,6 +9,8 @@ describe "StaticPages" do
   #   end
   # end
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do # 好きな文字列を指定できる
 
     it "should have the content 'Sample App'" do # RSpecはダブルクオートでくくられたものは無視する
@@ -18,7 +20,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -31,7 +33,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -44,7 +46,20 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
