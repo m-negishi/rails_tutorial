d6d1eaaebc458dc36e5ab147717acccfbb444e94
Masahiko.Negishi <masahiko.negishi@litalico.co.jp>
2015-06-02 15:03:58 +0900

    list 6.20

--- sample_app/spec/models/user_spec.rb
+++ sample_app/spec/models/user_spec.rb
@@ -51,4 +51,15 @@ describe User do
       end
     end
   end
+
+  # emailの一意性の保証
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
 end
