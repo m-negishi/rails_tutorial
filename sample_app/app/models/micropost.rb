cc90e4090eda0a3789f5c2a5cf8f51e79433e5f8
Masahiko.Negishi <masahiko.negishi@litalico.co.jp>
2015-06-09 19:36:57 +0900

    finish 10.1

--- sample_app/app/models/micropost.rb
+++ sample_app/app/models/micropost.rb
@@ -3,4 +3,5 @@ class Micropost < ActiveRecord::Base
   # -> {} の部分はブロック
   default_scope -> { order('created_at DESC') }
   validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
 end
