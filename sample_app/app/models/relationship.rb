d94970f7d9a6135da8fc0737425b03b7b29b0b85
Masahiko.Negishi <masahiko.negishi@litalico.co.jp>
2015-06-15 19:12:08 +0900

    finish 11.1

--- sample_app/app/models/relationship.rb
+++ sample_app/app/models/relationship.rb
@@ -4,4 +4,6 @@ class Relationship < ActiveRecord::Base
   # しかし、Followerモデルは存在しないので、参照するクラス名を明示する必要がある
   belongs_to :follower, class_name: 'User'
   belongs_to :followed, class_name: 'User'
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
 end
--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -4,6 +4,13 @@ class User < ActiveRecord::Base
   # ここではユーザを扱っているが、relationshipsのテーブルのユーザはuser_idではなく、
   # 外部キーであるfollower_idによって特定されるので、明示的に示す
   has_many :relationships, foreign_key: 'follower_id', dependent: :destroy
+
+  # has_many :followed_users, through: :relationships
+  # このコードはrelationshipsテーブルのfollowed_idを使用して配列を作成する
+  # しかし、user.followedsとすると英語としておかしくなるので、user.followed_usersをフォローしているユーザの配列とする
+  # followed_users配列の元が、followed id の集合であることを明示するために、sourceで指定する
+  has_many :followed_users, through: :relationships, source: :followed
+
   before_save { self.email = email.downcase }
   # 上記のように明示的にブロックで渡しているが、以下のようにメソッド参照（メソッドを探す）する方が一般的
   before_create :create_remember_token
@@ -33,6 +40,22 @@ class User < ActiveRecord::Base
     # 疑問符があることで、idが確実にエスケープされる
     # Micropost.where("user_id = ?", id)
     Micropost.where(user_id: id)
+  end
+
+  # あるユーザが別のあるユーザをフォローしているかチェック
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  # あるユーザが別のあるユーザをフォローする（フォロー関係をcreate!する）
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+    # 上記は、下記と同様
+    # self.relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
   end
 
   private
