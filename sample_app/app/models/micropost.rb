a5b14b5445e7b73cc94db56f80d6f3e35a10d15f
Masahiko.Negishi <masahiko.negishi@litalico.co.jp>
2015-06-17 14:12:42 +0900

    finish 11.3

--- sample_app/app/models/micropost.rb
+++ sample_app/app/models/micropost.rb
@@ -5,9 +5,18 @@ class Micropost < ActiveRecord::Base
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 
+  # 与えられたユーザがフォローしているユーザ達のマイクロポストを返す
   def self.from_users_followed_by(user)
     # followed_user_idsメソッドは、has_many: :followed_usersから自動生成される
-    followed_user_ids = user.followed_user_ids
-    where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+    #
+    # なんでSQLで書きなおすのかわかってない
+    # 集合のロジックをDBに保存するから効率がいい？
+    # followed_user_ids = user.followed_user_ids
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    # ?を内挿すると自動的に色々補完してくれる
+    # where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+    # where("user_id IN (:followed_user_ids) OR user_id = :user_id", followed_user_ids: followed_user_ids, user_id: user)
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", user_id: user.id)
   end
 end
